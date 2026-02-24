8305fde49d6c6a151de6dacffca55b6aacc4fc99
Cristian <cristianrodriguezlugo@gmail.com>
2026-02-24 08:54:15 +0100

    Simplificado proyecto eliminando dependencias no usadas

---
+++ calculator-app/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>Calculator App</Text>
+    </View>
+  );
+};
+
+export default CalculatorApp;
